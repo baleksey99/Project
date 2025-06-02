4c2c82a883968ecc4773cb8c4c402f01ff2f6f50
Алексей <abe26@yandex.ru>
2025-06-02 20:50:13 +0300

    final commit

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def get_mask_card_number(number_card: List[str]) -> str:
-    '''Функция, которая принимает номер карты в виде числа и возвращает маску   '''
+    """Функция, которая принимает номер карты в виде числа и возвращает маску"""
     number_card_1 = "".join(number_card)
     number_card_1 = number_card_1.replace(" ", "")
     mask_card = " ".join(number_card_1[i : i + 4] for i in range(0, len(number_card), 4))
@@ -15,14 +15,8 @@ def get_mask_card_number(number_card: List[str]) -> str:
     return mask_card_number
 
 
-
-
-
 def get_mask_account(number_cart):
-    '''Функция, которая принимает номер счета в виде числа и возвращает маску'''
+    """Функция, которая принимает номер счета в виде числа и возвращает маску"""
     number_cart = number_cart.replace(" ", "")
     number_mask = str(number_cart[-4:])
     return f"**{number_mask}"
-
-
-
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
-from typing import List
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info: str) -> str:
@@ -8,22 +7,22 @@ def mask_account_card(info: str) -> str:
     type_info = " ".join(parts[:-1])
     number = parts[-1]
 
-    if 'счет' in type_info.lower():
+    if "счет" in type_info.lower():
         masked_number = get_mask_account(number)
     else:
         masked_number = get_mask_card_number(list(number))
 
     return f"{type_info} {masked_number}"
 
+
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
 
 
 def get_date(info: str) -> str:
-    '''Функция, которая возвращает строку с датой в формате "ДД.ММ.ГГГГ" '''
+    """Функция, которая возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
     date = f"{info[8:10]}.{info[5:7]}.{info[0:4]}"
     return date
 
+
 print(get_date("2024-03-11T02:26:18.671407"))
-
-
