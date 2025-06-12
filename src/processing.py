a600f69d288b1d1cf17671b2490909ea4591f2b7
Алексей <abe26@yandex.ru>
2025-06-12 23:23:25 +0300

    final commit

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,10 @@
-def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
+from typing import List, Dict
+from datetime import datetime
+
+
+def filter_by_state(list_of_dict: List[Dict[str, str]], state='EXECUTED') -> List[Dict[str, str]]:
+    """Функция возвращащая список словарей, содержащий только те
+     словари, у которых ключ state соответствует указанному значению"""
     filtered_list = []
     for dict in list_of_dict:
         for key, value in dict.items():
@@ -8,13 +14,12 @@ def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
                 continue
     return filtered_list
 
+
 result = filter_by_state(list_of_dict=eval(input("Enter the list of dictionaries: ")))
 print(result)
 
 
-from typing import List, Dict
-
 def sort_by_date(list_of_dict: List[Dict[str, str]], reverse: bool = True) -> List[Dict[str, str]]:
-    """Сортирует список словарей по полю 'date' (дате)."""
+    """Сортирует список словарей по полю 'date' (дата)."""
     sorted_list_of_dict = sorted(list_of_dict, key=lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
     return sorted_list_of_dict
