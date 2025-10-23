0afef3934c55edb7315342c4b2f61acb05cdcc17
Алексей <abe26@yandex.ru>
2025-10-23 23:21:08 +0300

    Home7

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def filter_transactions_by_currency(input_file):
@@ -6,11 +14,14 @@ def filter_transactions_by_currency(input_file):
     список словарей с данными о финансовых транзакциях"""
 
     try:
+        logger.info("Запрашиваем информацию из файла")
         with open(input_file, 'r', encoding='utf-8') as f:
             transactions = json.load(f)
             if not isinstance(transactions, list):
+                logger.warning("В файле отсутствует список")
                 raise TypeError("JSON-файл должен содержать список.")
             return transactions
-    except (FileNotFoundError, json.JSONDecodeError, TypeError) as e:
-        print(f"Ошибка: {e}")
+    except (FileNotFoundError, json.JSONDecodeError, TypeError) as ex:
+        print(f"Ошибка: {ex}")
+        logger.error(f"Произошла ошибка: {ex}")
         return []
